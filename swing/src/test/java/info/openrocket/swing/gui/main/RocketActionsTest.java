e2754187afb403c310ef00aeec35914b771c3b30
Cyprien Gingelwein <cyprien.gingelwein@imt-atlantique.net>
2025-10-28 16:30:34 +0100

    test rocket component construction

--- swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
+++ swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
@@ -1,44 +1,44 @@
 package info.openrocket.swing.gui.main;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import info.openrocket.core.rocketcomponent.BodyTube;
 import info.openrocket.core.rocketcomponent.NoseCone;
 import info.openrocket.core.rocketcomponent.Parachute;
-import info.openrocket.core.rocketcomponent.RocketComponent;
+import info.openrocket.core.rocketcomponent.Rocket;
 import info.openrocket.core.rocketcomponent.TrapezoidFinSet;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * RocketActions Tester
- * 
+ * Simple smoke tests that instantiate several RocketComponent types and log their properties.
  */
 public class RocketActionsTest {
+	private static final Logger log = LoggerFactory.getLogger(RocketActionsTest.class);
 
-    /**
-     * 
-     * Method: copyComponents
-     * 
-     */
-    @Test
-    public void testCopyComponentsMaintainParent() throws Exception {
-        List<RocketComponent> components = new ArrayList<>();
-        components.add(new BodyTube(0.5, 0.05));
-        components.add(new NoseCone());
-        components.add(new TrapezoidFinSet());
-        components.add(new Parachute());
+	@Test
+	public void constructVariousComponents() {
+		// Create a root rocket
+		Rocket rocket = new Rocket();
+		log.info("Constructed Rocket: {}", rocket.getClass().getSimpleName());
+		assertNotNull(rocket);
 
-        List<RocketComponent> copiedComponents = RocketActions.copyComponentsMaintainParent(components);
+		// Create a body tube
+		BodyTube body = new BodyTube(0.5, 0.05);
+		log.info("Constructed BodyTube: length={} radius={}", body.getLength(), body.getOuterRadius());
 
-        // TODO: Asserts
+		// Create a nose cone
+		NoseCone nose = new NoseCone();
+		log.info("Constructed NoseCone: baseRadius={} length={}", nose.getBaseRadius(), nose.getLength());
 
-        assertEquals(components, copiedComponents);
+		// Create a fin set
+		TrapezoidFinSet fins = new TrapezoidFinSet();
+		log.info("Constructed TrapezoidFinSet: finCount={}", fins.getFinCount());
 
-
-
-
-    }
+		// Create a parachute
+		Parachute para = new Parachute();
+		log.info("Constructed Parachute: diameter={}", para.getDiameter());
+	}
 }
