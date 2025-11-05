bc02be22e2a1cd090de819d7dc0e67a7332750e1
Julien ORSONI <181235150+jorsoni@users.noreply.github.com>
2025-11-05 15:14:41 +0100

    Fix The Problem

--- swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
+++ swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
@@ -1,40 +1,37 @@
 package info.openrocket.swing.gui.main;
 
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import info.openrocket.core.rocketcomponent.BodyTube;
 import info.openrocket.core.rocketcomponent.NoseCone;
 import info.openrocket.core.rocketcomponent.Parachute;
-import info.openrocket.core.rocketcomponent.Rocket;
+import info.openrocket.core.rocketcomponent.RocketComponent;
 import info.openrocket.core.rocketcomponent.TrapezoidFinSet;
-import info.openrocket.core.rocketcomponent.NoseCone.Shape;
 
 /**
- * Simple smoke tests that instantiate several RocketComponent types and log their properties.
+ * RocketActions Tester
+ * 
  */
 public class RocketActionsTest {
-	private static final Logger log = LoggerFactory.getLogger(RocketActionsTest.class);
 
-	@Test
-	public void constructVariousComponents() {
-		// Create a body tube
-		BodyTube body = new BodyTube(0.5, 0.05);
-		log.info("Constructed BodyTube: length={} radius={}", body.getLength(), body.getOuterRadius());
+    /**
+     * 
+     * Method: copyComponents
+     * 
+     */
+    @Test
+    public void testCopyComponentsMaintainParent() throws Exception {
+        List<RocketComponent> components = new ArrayList<>();
+        components.add(new BodyTube(0.5, 0.05));
+        components.add(new NoseCone(NoseCone.Shape.CONICAL, 6 * NoseCone.DEFAULT_RADIUS, NoseCone.DEFAULT_RADIUS));
+        components.add(new TrapezoidFinSet(3, 0.05, 0.05, 0.025, 0.03));
+        components.add(new Parachute());
 
-		// Create a nose cone
-		NoseCone nose = new NoseCone(NoseCone.Shape.CONICAL, 6 * NoseCone.DEFAULT_RADIUS, NoseCone.DEFAULT_RADIUS);
-		log.info("Constructed NoseCone: baseRadius={} length={}", nose.getBaseRadius(), nose.getLength());
+        List<RocketComponent> copiedComponents = RocketActions.copyComponentsMaintainParent(components);
 
-		// Create a fin set
-		TrapezoidFinSet fins = new TrapezoidFinSet(3, 0.05, 0.05, 0.025, 0.03);
-		log.info("Constructed TrapezoidFinSet: finCount={}", fins.getFinCount());
-
-		// Create a parachute
-		Parachute para = new Parachute();
-		log.info("Constructed Parachute: diameter={}", para.getDiameter());
-	}
+        // TODO: Asserts
+    }
 }
