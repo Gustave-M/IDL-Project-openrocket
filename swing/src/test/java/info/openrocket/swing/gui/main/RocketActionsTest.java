06d879dafb53ce237b5d7138a6b074d137f3ef74
Julien ORSONI <181235150+jorsoni@users.noreply.github.com>
2025-11-05 13:09:36 +0100

    Create and copy components

--- swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
+++ swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
@@ -1,5 +1,37 @@
 package info.openrocket.swing.gui.main;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import info.openrocket.core.rocketcomponent.BodyTube;
+import info.openrocket.core.rocketcomponent.NoseCone;
+import info.openrocket.core.rocketcomponent.Parachute;
+import info.openrocket.core.rocketcomponent.RocketComponent;
+import info.openrocket.core.rocketcomponent.TrapezoidFinSet;
+
+/**
+ * RocketActions Tester
+ * 
+ */
 public class RocketActionsTest {
-    
+
+    /**
+     * 
+     * Method: copyComponents
+     * 
+     */
+    @Test
+    public void testCopyComponentsMaintainParent() throws Exception {
+        List<RocketComponent> components = new ArrayList<>();
+        components.add(new BodyTube(0.5, 0.05));
+        components.add(new NoseCone());
+        components.add(new TrapezoidFinSet());
+        components.add(new Parachute());
+
+        List<RocketComponent> copiedComponents = RocketActions.copyComponentsMaintainParent(components);
+
+        // TODO: Asserts
+    }
 }
