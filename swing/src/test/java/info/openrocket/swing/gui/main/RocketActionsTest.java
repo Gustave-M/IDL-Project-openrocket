7dd51e30ad0c6ad93435d20b5cb2dae8a6224572
Cyprien Gingelwein <cyprien.gingelwein@imt-atlantique.net>
2025-11-05 15:04:23 +0100

    add parameters to rocketcomponant

--- swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
+++ swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
@@ -26,8 +26,8 @@ public class RocketActionsTest {
     public void testCopyComponentsMaintainParent() throws Exception {
         List<RocketComponent> components = new ArrayList<>();
         components.add(new BodyTube(0.5, 0.05));
-        components.add(new NoseCone());
-        components.add(new TrapezoidFinSet());
+        components.add(new NoseCone(NoseCone.Shape.CONICAL, 6 * NoseCone.DEFAULT_RADIUS, NoseCone.DEFAULT_RADIUS));
+        components.add(new TrapezoidFinSet(3, 0.05, 0.05, 0.025, 0.03));
         components.add(new Parachute());
 
         List<RocketComponent> copiedComponents = RocketActions.copyComponentsMaintainParent(components);
