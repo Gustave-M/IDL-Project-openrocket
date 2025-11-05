9db054a60412517a759d2c3423a60d4789da8672
Cyprien Gingelwein <cyprien.gingelwein@imt-atlantique.net>
2025-11-05 14:22:51 +0100

    testing rocket component creation

--- swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
+++ swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
@@ -11,6 +11,7 @@ import info.openrocket.core.rocketcomponent.NoseCone;
 import info.openrocket.core.rocketcomponent.Parachute;
 import info.openrocket.core.rocketcomponent.Rocket;
 import info.openrocket.core.rocketcomponent.TrapezoidFinSet;
+import info.openrocket.core.rocketcomponent.NoseCone.Shape;
 
 /**
  * Simple smoke tests that instantiate several RocketComponent types and log their properties.
@@ -20,21 +21,16 @@ public class RocketActionsTest {
 
 	@Test
 	public void constructVariousComponents() {
-		// Create a root rocket
-		Rocket rocket = new Rocket();
-		log.info("Constructed Rocket: {}", rocket.getClass().getSimpleName());
-		assertNotNull(rocket);
-
 		// Create a body tube
 		BodyTube body = new BodyTube(0.5, 0.05);
 		log.info("Constructed BodyTube: length={} radius={}", body.getLength(), body.getOuterRadius());
 
 		// Create a nose cone
-		NoseCone nose = new NoseCone();
+		NoseCone nose = new NoseCone(NoseCone.Shape.CONICAL, 6 * NoseCone.DEFAULT_RADIUS, NoseCone.DEFAULT_RADIUS);
 		log.info("Constructed NoseCone: baseRadius={} length={}", nose.getBaseRadius(), nose.getLength());
 
 		// Create a fin set
-		TrapezoidFinSet fins = new TrapezoidFinSet();
+		TrapezoidFinSet fins = new TrapezoidFinSet(3, 0.05, 0.05, 0.025, 0.03);
 		log.info("Constructed TrapezoidFinSet: finCount={}", fins.getFinCount());
 
 		// Create a parachute
