0d97f34f0e917f92a88aac55e807a708c46d42e2
raphael hierso iglesias <raphael.hierso-iglesias@imt-atlantique>
2025-11-16 08:05:27 +0100

    Adding assertion for copy action test file

--- swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
+++ swing/src/test/java/info/openrocket/swing/gui/main/RocketActionsTest.java
@@ -33,5 +33,38 @@ public class RocketActionsTest {
         List<RocketComponent> copiedComponents = RocketActions.copyComponentsMaintainParent(components);
 
         // TODO: Asserts
+        @Test
+        assertEquals(components.size(), copiedComponents.size());
+        assertEquals(components.get(0).getClass(), copiedComponents.get(0).getClass());
+        assertEquals(components.get(1).getClass(), copiedComponents.get(1).getClass());
+        assertEquals(components.get(2).getClass(), copiedComponents.get(2).getClass());
+        assertEquals(components.get(3).getClass(), copiedComponents.get(3).getClass());
+
+        assertEquals(components.get(0).getLength(), copiedComponents.get(0).getLength());
+        assertEquals(components.get(0).getRadius(), copiedComponents.get(0).getRadius());
+        assertEquals(components.get(1).getLength(), copiedComponents.get(1).getLength());
+        assertEquals(components.get(1).getRadius(), copiedComponents.get(1).getRadius());
+        assertEquals(components.get(2).getRootChord(), copiedComponents.get(2).getRootChord());
+        assertEquals(components.get(2).getTipChord(), copiedComponents.get(2).getTipChord());
+        assertEquals(components.get(2).getSweep(), copiedComponents.get(2).getSweep());
+        assertEquals(components.get(2).getHeight(), copiedComponents.get(2).getHeight());
+        assertEquals(components.get(3).getDiameter(), copiedComponents.get(3).getDiameter());
+        assertEquals(components.get(3).getArea(), copiedComponents.get(3).getArea());
+
+
+        // Copies should be distinct objects from originals
+        assertNotSame(components.get(0), copiedComponents.get(0));
+        assertNotSame(components.get(1), copiedComponents.get(1));
+        assertNotSame(components.get(2), copiedComponents.get(2));
+        assertNotSame(components.get(3), copiedComponents.get(3));
+
+
+        //Todo : parent-child relationship tests
+
+        //Todo : test for paste operation
+
+        // Todo : test for duplicate operation
+
+        // Todo : test for cut operation
     }
 }
